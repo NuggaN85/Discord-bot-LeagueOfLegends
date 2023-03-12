fad558bdd126f10e9f1a69f87046c8ff2fd0133e
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-03-12 20:14:39 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+const { Client, Intents } = require('discord.js');
+const axios = require('axios');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const TOKEN = 'votre-token-de-bot';
+const API_KEY = 'votre-clef-d-api-tracker.gg';
+
+client.on('ready', () => {
+  console.log(`Connecté en tant que ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'stats') {
+    const player = interaction.options.getString('joueur');
+
+    try {
+      const response = await axios.get(`https://api.tracker.gg/api/v2/valorant/standard/profile/riot/${player}`, {
+        headers: { 'TRN-Api-Key': API_KEY }
+      });
+
+      const { data } = response;
+
+      // Formatez les données de la réponse en utilisant Discord's MessageEmbed ou un simple message textuel
+      const statsEmbed = new Discord.MessageEmbed()
+        .setTitle(`Statistiques de ${player}`)
+        .addField('Kills', data.data.segments[0].stats.kills.value, true)
+        .addField('Morts', data.data.segments[0].stats.deaths.value, true)
+        .addField('Assists', data.data.segments[0].stats.assists.value, true);
+
+      await interaction.reply({ embeds: [statsEmbed] });
+    } catch (error) {
+      console.error(error);
+      await interaction.reply('Erreur lors de la récupération des statistiques du joueur.');
+    }
+  }
+});
+
+client.login(TOKEN);
